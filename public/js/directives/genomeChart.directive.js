93fc54f1c58d66ad00818608b62a75543d91f04d
Paul Jones <paulvjones@gmail.com>
2017-03-03 15:50:32 -0700

    more progress with genome bar

--- public/js/directives/genomeChart.directive.js
+++ public/js/directives/genomeChart.directive.js
@@ -18,187 +18,124 @@
           window.onresize = (event) => {
             width = ChartResizeService.calculateElementWidth(chartElement)
             updateChart()
-          }
+          };
 
-          var margins = {
+          scope.$watch("data", (n, o) => {
+            if (n !== o) {
+              updateChart();
+            }
+          });
+
+          let margins = {
             top: 5,
             left: 5,
             right: 5,
             bottom: 5
           };
 
-          var height = 40 - margins.top - margins.bottom;
+          let height = 40 - margins.top - margins.bottom;
 
-          var chromosomeRef = {
-            1: 249250621,
-            2: 243199373,
-            3: 198022430,
-            4: 191154276,
-            5: 180915260,
-            6: 171115067,
-            7: 159138663,
-            8: 146364022,
-            9: 141213431,
-            10: 135534747,
-            11: 135006516,
-            12: 133851895,
-            13: 115169878,
-            14: 107349540,
-            15: 102531392,
-            16: 90354753,
-            17: 81195210,
-            18: 78077248,
-            19: 59128983,
-            20: 63025520,
-            21: 48129895,
-            22: 51304566,
-            23: 155270560
-          }
-
-          var dataset = [{
-
-            data: [{
+          let dataset = [
+            [{
               id: 1,
               length: 249250621
             }],
-            name: 'chromosome1'
-          }, {
-            data: [{
+            [{
               id: 2,
               length: 243199373
             }],
-            name: 'chromosome2'
-          }, {
-            data: [{
+            [{
               id: 3,
               length: 198022430
             }],
-            name: 'chromosome3'
-          }, {
-            data: [{
+            [{
               id: 4,
               length: 191154276
             }],
-            name: 'chromosome4'
-          }, {
-            data: [{
+            [{
               id: 5,
               length: 180915260
             }],
-            name: 'chromosome5'
-          }, {
-            data: [{
+            [{
               id: 6,
               length: 171115067
             }],
-            name: 'chromosome6'
-          }, {
-            data: [{
+            [{
               id: 7,
               length: 159138663
             }],
-            name: 'chromosome7'
-          }, {
-            data: [{
+            [{
               id: 8,
               length: 146364022
             }],
-            name: 'chromosome8'
-          }, {
-            data: [{
+            [{
               id: 9,
               length: 141213431
             }],
-            name: 'chromosome9'
-          }, {
-            data: [{
+            [{
               id: 10,
               length: 135534747
             }],
-            name: 'chromosome10'
-          }, {
-            data: [{
+            [{
               id: 11,
               length: 135006516
             }],
-            name: 'chromosome11'
-          }, {
-            data: [{
+            [{
               id: 12,
               length: 133851895
             }],
-            name: 'chromosome12'
-          }, {
-            data: [{
+            [{
               id: 13,
               length: 115169878
             }],
-            name: 'chromosome13'
-          }, {
-            data: [{
+            [{
               id: 14,
               length: 107349540
             }],
-            name: 'chromosome14'
-          }, {
-            data: [{
+            [{
               id: 15,
               length: 102531392
             }],
-            name: 'chromosome15'
-          }, {
-            data: [{
+            [{
               id: 16,
               length: 90354753
             }],
-            name: 'chromosome16'
-          }, {
-            data: [{
+            [{
               id: 17,
               length: 81195210
             }],
-            name: 'chromosome17'
-          }, {
-            data: [{
+            [{
               id: 18,
               length: 78077248
             }],
-            name: 'chromosome18'
-          }, {
-            data: [{
+            [{
               id: 19,
               length: 59128983
             }],
-            name: 'chromosome19'
-          }, {
-            data: [{
+            [{
               id: 20,
               length: 63025520
             }],
-            name: 'chromosome20'
-          }, {
-            data: [{
+            [{
               id: 21,
               length: 48129895
             }],
-            name: 'chromosome21'
-          }, {
-            data: [{
+            [{
               id: 22,
               length: 51304566
             }],
-            name: 'chromosome22'
-          }, {
-            data: [{
+            [{
               id: 23,
               length: 155270560
-            }],
-            name: 'chromosome23'
-          }]
+            }]
+          ]
 
-          var dataset = dataset.map(function(d) {
-            return d.data.map(function(e, i) {
-              // Structure it so that your numeric
+          // DATA FORMATTING (SPECIFIC TO D3 V3)
+          // GENOME/CHROMOSOMES
+
+          let datasetMod1 = dataset.map(function(elem) {
+            return elem.map(function(e, i) {
+              // Structure it so that numeric
               // axis (the stacked amount) is y
               return {
                 y: e.length,
@@ -207,11 +144,10 @@
             });
           });
 
-          var stack = d3.layout.stack();
+          let stack = d3.layout.stack();
+          stack(datasetMod1);
 
-          stack(dataset);
-
-          var dataset = dataset.map(function(group) {
+          let datasetMod2 = datasetMod1.map(function(group) {
             return group.map(function(d) {
               // Invert the x and y values, and y0 becomes x0
               return {
@@ -222,41 +158,38 @@
             });
           });
 
-          scope.$watch("data", (n, o) => {
-            if (n !== o) {
-              updateChart();
-            }
-          })
+          let dataTEST = [{
+            name: 'foo',
+            value: 100000000
+          }, {
+            name: 'bar',
+            value: 200000000
+          }]
+
+          var tip = d3.tip()
+            .attr('class', 'd3-tip')
+            .html(function(d) {
+              return `<span class='tooltip-rsid'>RSID: ${d.rsid}</span><br /><span class='tooltip-chromosome'>Chromosome:  ${d.chromosome}</span><br /><span class='tooltip-position'>Positon:  ${d.position}</span>`;
+            })
+
+
+          //DRAW CHART
 
           function updateChart() {
+
+            let snpVals = getSnpVals(scope.data);
 
             d3.select(elem[0]).selectAll('svg').remove()
 
             let svg = d3.select(elem[0])
               .append('svg')
               .attr('width', width + margins.left + margins.right)
-              .attr('height', height + margins.bottom + margins.top)
-              .append('g')
-              .attr('transform', `translate(${margins.left},${margins.top})`)
+              .attr('height', height + 100 + margins.bottom + margins.top)
 
-            let snpVals = []
+          
 
-            scope.data.forEach((elem) => {
-                let position = 0;
-
-                let chromosomeNums = []
-                for (let i = 1; i <= elem.chromosome; i++) {
-                  chromosomeNums.push(i)
-                }
-                chromosomeNums.forEach((elem) => {
-                  position += chromosomeRef[elem];
-                })
-                position += +elem.position;
-                snpVals.push(position)
-              }) //END OF FOREACH
-
-            let xMax = d3.max(dataset, function(group) {
-              return d3.max(group, function(d) {
+            let xMax = d3.max(datasetMod2, (group) => {
+              return d3.max(group, (d) => {
                 return d.x + d.x0;
               });
             });
@@ -270,50 +203,140 @@
               .interpolate(d3.interpolateHcl)
               .range([d3.rgb("#104f99"), d3.rgb('#f75050')]);
 
-            let groups = svg.selectAll('g')
-              .data(dataset);
+            let series = svg.append('g')
+              .attr('transform', `translate(${margins.left},${margins.top})`)
+              .selectAll('g')
+              .data(datasetMod2);
 
-            groups.enter().append('g')
-              .style('fill', function(d, i) {
+            series.enter().append('g')
+              .style('fill', (d, i) => {
                 return colors(i);
               })
               .attr('fill-opacity', 0.6)
-              .style('stroke', function(d, i) {
+              .style('stroke', (d, i) => {
                 return colors(i);
               })
               .style('stroke-width', 1)
 
             d3.selectAll('rect').remove()
 
-            let rects = groups.selectAll('rect')
-              .data(function(d) {
+            let rects = series.selectAll('rect')
+              .data((d) => {
                 return d;
               });
 
             rects.enter().append('rect')
-              .attr('x', function(d) {
+              .attr('x', (d) => {
                 return xScale(d.x0);
               })
               .attr('height', 30)
-              .attr('width', function(d) {
+              .attr('width', (d) => {
                 return xScale(d.x) - 3;
+              }) 
+
+
+            /////////
+
+            let series2 = svg.append('g')
+              .selectAll('g')
+              .data(snpVals);
+
+            series2.enter().append('g')
+              .attr('fill', 'red')
+              .attr('stroke', '#004a71')
+              .attr('stroke-width', 1)
+              .attr('opacity', 0.8)
+
+            let rects2 = series2.selectAll('rect')
+              .data(snpVals);
+
+            rects2.enter().append('rect')
+              .attr('class', 'snp-line')
+              .attr('x', (d) => {
+                return xScale(d.totalPosition)
               })
+              .attr('height', 40)
+              .attr('width', 4)
+              
+            // rects2.enter().append("text")
+            //   .attr("class", "map-text")
+            //   .attr("x", (d) => {
+            //     return xScale(d.totalPosition) + 5
+            //   })
+            //   .attr("y", 45)
+            //   .attr("dy", ".35em")
+            //   .text((d) => {
+            //     return d.rsid;
+            //   })
 
-            rects.exit().remove();
+            d3.selectAll('.snp-line').call(tip)
+            d3.selectAll('.snp-line')
+              .on('mouseover', tip.show)
+              .on('mouseout', tip.hide)
 
-            d3.selectAll('.snp-line').remove()
-            snpVals.forEach(function(elem) {
+            // overlap();
 
-              svg.append('rect')
-                .attr('fill', 'red')
-                .attr('width', 3)
-                .attr('height', 40)
-                .attr('x', xScale(elem))
-                .attr('y', -5)
-                .classed('snp-line', true)
-            })
+          }; //END OF UPDATE FUNCTION
 
-          }
+          //UTILTIY FUNCTIONS
+
+          function calculateTextPosition() {}
+
+          function getSnpVals(snpArray) {
+            let snpVals = snpArray.map((elem) => {
+              let snp = elem;
+              snp.totalPosition = calculateSnpPosition(elem);
+              return snp;
+            });
+            console.log(snpVals);
+            return snpVals;
+          };
+
+          function calculateSnpPosition(snpObj) {
+            let position = 0;
+            for (let i = 0; i <= snpObj.chromosome - 1; i++) {
+              position += +dataset[i][0].length;
+            }
+            position += +snpObj.position;
+            return position;
+          };
+
+          // function overlap() {
+          //   var move = 1;
+          //   while (move > 0) {
+          //     move=0;
+          //     d3.selectAll('.map-text')
+          //       .each(function() {
+          //         var that = this,
+          //           a = this.getBoundingClientRect();
+          //         console.log(a)
+          //         d3.selectAll('.map-text')
+          //           .each(function() {
+          //             if (this != that) {
+          //               var b = this.getBoundingClientRect();
+          //               if ((Math.abs(a.left - b.left) * 2 < (a.width + b.width)) &&
+          //                 (Math.abs(a.top - b.top) * 2 < (a.height + b.height))) {
+          //                 // overlap, move labels
+          //                 var dx = (Math.max(0, a.right - b.left) +
+          //                     Math.min(0, a.left - b.right)) * 0.01,
+          //                   dy = (Math.max(0, a.bottom - b.top) +
+          //                     Math.min(0, a.top - b.bottom)) * 0.02,
+          //                   tt = d3.transform(d3.select(this).attr("transform")),
+          //                   to = d3.transform(d3.select(that).attr("transform"));
+          //                 move += Math.abs(dx) + Math.abs(dy);
+
+          //                 to.translate = [to.translate[0] + dx, to.translate[1] + dy];
+          //                 tt.translate = [tt.translate[0] - dx, tt.translate[1] - dy];
+          //                 d3.select(this).attr("transform", "translate(" + tt.translate + ")");
+          //                 d3.select(that).attr("transform", "translate(" + to.translate + ")");
+          //                 a = this.getBoundingClientRect();
+          //               }
+          //             }
+          //           })
+          //       })
+          //   }
+          // }
+
         } //end of link
     };
   };
